b64b50be2493e6ddad41bd20c8c6dff317c9ab9e
PetrRyb <petr.rybin@infis.cz>
2025-05-06 09:37:42 +0200

    Add files via upload

---
+++ dictionary+funkce.py
@@ -0,0 +1,7 @@
+vytvor_utulek = {
+   "jmeno": "Kačka" , "věk": 3, "druh" : "kočka"   
+}
+print(vytvor_utulek)
+pridej_zvire = ("Napište sem útulek, jméno, druh a věk zvířete, které chcete přidat"):
+    
+vypis_zvirata = print(f" {vytvor_utulek ["jmeno"]} je {vytvor_utulek ["druh"]} a je jí {vytvor_utulek ["věk"]}")
